ea3ee1e6afbc929a185e7fa0e56dfa0707194470
Kayle Gishen <kayle.gishen@getbuilt.com>
2023-10-23 12:59:04 -0400

    fix: use gitcommit file type to get conventional commit cmp

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -1,7 +1,14 @@
 vim.filetype.add({
-	extension = {
-		prisma = function(path, bufnr)
-			return 'prisma'
-		end
-	},
+  extension = {
+    prisma = function(path, bufnr)
+      return 'prisma'
+    end
+  },
+})
+
+vim.api.nvim_create_augroup("neogit-additions", {})
+vim.api.nvim_create_autocmd("FileType", {
+  group = "neogit-additions",
+  pattern = "NeogitCommitMessage",
+  command = "silent! set filetype=gitcommit",
 })
