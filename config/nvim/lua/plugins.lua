f6facfa27e44ef3f9e9c6a6d5e656031488f274b
Kayle Gishen <kayle.gishen@getbuilt.com>
2023-12-14 20:57:11 -0500

    fix: setup required snippet engine for code completion

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -691,7 +691,8 @@ require("lazy").setup({
       { "hrsh7th/cmp-nvim-lsp-signature-help" },
       { "hrsh7th/cmp-path" },
       { "petertriho/cmp-git" },
-      -- { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/vim-vsnip' }
     },
     config = function()
       local cmp = require("cmp")
@@ -700,7 +701,7 @@ require("lazy").setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -734,7 +735,7 @@ require("lazy").setup({
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" },
-          -- { name = 'vsnip' }, -- For vsnip users.
+          { name = 'vsnip' }, -- For vsnip users.
           -- { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
