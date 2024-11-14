2b62e304069c056cec42d240fd3f3b08141acea8
Kayle Gishen <kaylegishen@gmail.com>
2024-11-14 11:06:29 -0500

    feat: add new file shortcut to dashboard

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -384,12 +384,26 @@ require("lazy").setup({
   {
     'nvimdev/dashboard-nvim',
     event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+    opts = {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = ' New File', group = '@property', action = 'enew', key = 'n' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+        },
+      },
+    }
   },
   {
     "nvim-tree/nvim-tree.lua",
