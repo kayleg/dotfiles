b9c6cc9ca66c718165c089f7a5fdf05c405a38ef
Kayle Gishen <kayle.gishen@getbuilt.com>
2025-03-12 11:06:00 -0400

    feat: add gotmpl syntax

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,6 +35,20 @@ vim.diagnostic.config({
   virtual_lines = { only_current_line = true }
 })
 
+-- Go Tmpl Syntax
+vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
+  local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+  local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
+  metadata["injection.language"] = ext
+end, {})
+
+vim.filetype.add({
+  extension = {
+    tmpl = "gotmpl",
+    gotmpl = "gotmpl",
+  },
+})
+
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
